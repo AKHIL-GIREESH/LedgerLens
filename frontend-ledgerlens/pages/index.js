8c3153a9e0899efe8fbf774e1d20ae6bb8edbb90
Anantha-K <89961607+Anantha-K@users.noreply.github.com>
2023-05-05 00:43:14 +0530

    Update index.js

--- frontend-ledgerlens/pages/index.js
+++ frontend-ledgerlens/pages/index.js
@@ -14,15 +14,25 @@ console.log(connectionStatus)
     (connectionStatus === "connected")?
    <>
 <Navbar/>
-<section className="dark:bg-gray-800 dark:text-gray-100 bg-black text-white">
-	<div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
+<section className=" dark:text-gray-100 bg-black text-white">
+	<div className="container max-w-6xl py-6 px-0 mx-auto space-y-6 sm:space-y-12">
 
 		<div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
-    <a href="#" className="group relative block bg-black">
+    <motion.div
+      className="box"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}
+    >
+    <Link href="/Article" className="group relative block bg-black rounded-3xl my-2 mx-4">
   <img
     alt="Developer"
     src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
-    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50 rounded-3xl"
   />
 
   <div className="relative p-4 sm:p-6 lg:p-8">
@@ -44,13 +54,23 @@ console.log(connectionStatus)
       </div>
     </div>
   </div>
-</a>
-
-<a href="#" className="group relative block bg-black">
+</Link>
+</motion.div>  
+<motion.div
+      className="box"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}
+    >
+    <Link href="/Article" className="group relative block bg-black rounded-3xl my-2 mx-4">
   <img
     alt="Developer"
     src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
-    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50 rounded-3xl"
   />
 
   <div className="relative p-4 sm:p-6 lg:p-8">
@@ -72,12 +92,23 @@ console.log(connectionStatus)
       </div>
     </div>
   </div>
-</a>
-<a href="#" className="group relative block bg-black">
+</Link>
+</motion.div>  
+<motion.div
+      className="box"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}
+    >
+    <Link href="/Article" className="group relative block bg-black rounded-3xl my-2 mx-4">
   <img
     alt="Developer"
     src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
-    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50 rounded-3xl"
   />
 
   <div className="relative p-4 sm:p-6 lg:p-8">
@@ -99,12 +130,23 @@ console.log(connectionStatus)
       </div>
     </div>
   </div>
-</a>
-<a href="#" className="group relative block bg-black">
+</Link>
+</motion.div>  
+<motion.div
+      className="box"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}
+    >
+    <Link href="/Article" className="group relative block bg-black rounded-3xl my-2 mx-4">
   <img
     alt="Developer"
     src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
-    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50 rounded-3xl"
   />
 
   <div className="relative p-4 sm:p-6 lg:p-8">
@@ -126,12 +168,23 @@ console.log(connectionStatus)
       </div>
     </div>
   </div>
-</a>
-<a href="#" className="group relative block bg-black">
+</Link>
+</motion.div>  
+<motion.div
+      className="box"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}
+    >
+    <Link href="/Article" className="group relative block bg-black rounded-3xl my-2 mx-4">
   <img
     alt="Developer"
     src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
-    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50 rounded-3xl"
   />
 
   <div className="relative p-4 sm:p-6 lg:p-8">
@@ -153,12 +206,23 @@ console.log(connectionStatus)
       </div>
     </div>
   </div>
-</a>
-<a href="#" className="group relative block bg-black">
+</Link>
+</motion.div>  
+<motion.div
+      className="box"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}
+    >
+    <Link href="/Article" className="group relative block bg-black rounded-3xl my-2 mx-4">
   <img
     alt="Developer"
     src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
-    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
+    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50 rounded-3xl"
   />
 
   <div className="relative p-4 sm:p-6 lg:p-8">
@@ -180,7 +244,128 @@ console.log(connectionStatus)
       </div>
     </div>
   </div>
-</a>
+</Link>
+</motion.div>  
+<motion.div
+      className="box"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}
+    >
+    <Link href="/Article" className="group relative block bg-black rounded-3xl my-2 mx-4">
+  <img
+    alt="Developer"
+    src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
+    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50 rounded-3xl"
+  />
+
+  <div className="relative p-4 sm:p-6 lg:p-8">
+    <p className="text-sm font-medium uppercase tracking-widest text-pink-500">
+      Developer
+    </p>
+
+    <p className="text-xl font-bold text-white sm:text-2xl">Tony Wayne</p>
+
+    <div className="mt-32 sm:mt-48 lg:mt-64">
+      <div
+        className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+      >
+        <p className="text-sm text-white">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+          perferendis hic asperiores quibusdam quidem voluptates doloremque
+          reiciendis nostrum harum. Repudiandae?
+        </p>
+      </div>
+    </div>
+  </div>
+</Link>
+</motion.div>  
+<motion.div
+      className="box"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}
+    >
+    <Link href="/Article" className="group relative block bg-black rounded-3xl my-2 mx-4">
+  <img
+    alt="Developer"
+    src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
+    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50 rounded-3xl"
+  />
+
+  <div className="relative p-4 sm:p-6 lg:p-8">
+    <p className="text-sm font-medium uppercase tracking-widest text-pink-500">
+      Developer
+    </p>
+
+    <p className="text-xl font-bold text-white sm:text-2xl">Tony Wayne</p>
+
+    <div className="mt-32 sm:mt-48 lg:mt-64">
+      <div
+        className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+      >
+        <p className="text-sm text-white">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+          perferendis hic asperiores quibusdam quidem voluptates doloremque
+          reiciendis nostrum harum. Repudiandae?
+        </p>
+      </div>
+    </div>
+  </div>
+</Link>
+</motion.div>  
+<motion.div
+      className="box"
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}
+    >
+    <Link href="/Article" className="group relative block bg-black rounded-3xl my-2 mx-4">
+  <img
+    alt="Developer"
+    src="https://images.unsplash.com/photo-1603871165848-0aa92c869fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80"
+    className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50 rounded-3xl"
+  />
+
+  <div className="relative p-4 sm:p-6 lg:p-8">
+    <p className="text-sm font-medium uppercase tracking-widest text-pink-500">
+      Developer
+    </p>
+
+    <p className="text-xl font-bold text-white sm:text-2xl">Tony Wayne</p>
+
+    <div className="mt-32 sm:mt-48 lg:mt-64">
+      <div
+        className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100"
+      >
+        <p className="text-sm text-white">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
+          perferendis hic asperiores quibusdam quidem voluptates doloremque
+          reiciendis nostrum harum. Repudiandae?
+        </p>
+      </div>
+    </div>
+  </div>
+</Link>
+</motion.div>  
+
+
+
+
+
+
 
 		</div>
 	
