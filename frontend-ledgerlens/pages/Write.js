281b1e5716b9a33669068bf570719d6a064bba3b
Akhil Gireesh <akhilgireeshakg@gmail.com>
2023-05-04 21:52:08 +0530

    IPFS integration

--- frontend-ledgerlens/pages/Write.js
+++ frontend-ledgerlens/pages/Write.js
@@ -1,15 +1,39 @@
-import React from 'react'
-import {useContract} from "@thirdweb-dev/react"
+import React, { useState } from 'react'
+import {useContract,useAddress, useStorageUpload} from "@thirdweb-dev/react"
 
 const Write = () => {
 
   const contract = useContract("0xFAB701E6c7BbeBB3AEFe265F457911B20f786ca7")
+  const address = useAddress()
+  
+  const [file,setFile] = useState()
+  const [title,setTitle] = useState("")
+  const [body,setBody] = useState("")
+  const {mutateAsync: upload} = useStorageUpload()
+
+  const uploadToIpfs = async () => {
+    const uploadURL = await upload({
+      data:[file],
+      options:{
+        uploadWithGatewayUrl:true,
+        uploadWithoutDirectory:true
+      }
+    })
+    
+    console.log("Upload URL",uploadURL)
+  }
+
   return (
     <>
       <input type="text" placeholder='Enter the Title'></input>
-      <input type="file"/>
+      <input type="file" onChange={(e) => {
+      if(e.target.files){
+        setFile(e.target.files)
+      }}
+      }
+      />
       <textarea placeholder='Start Typing'></textarea>
-      <button onClick={() => console.log("Submit")}>Submit</button>
+      <button onClick={() => uploadToIpfs()}>Submit</button>
     </>
   )
 }
