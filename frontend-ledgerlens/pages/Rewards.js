ca4a979d68811852f0d6ac6e6084d442a70a05fb
Anantha-K <89961607+Anantha-K@users.noreply.github.com>
2023-05-05 13:05:31 +0530

    Update Rewards.js

--- frontend-ledgerlens/pages/Rewards.js
+++ frontend-ledgerlens/pages/Rewards.js
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion'
+import { motion ,AnimatePresence} from 'framer-motion'
 import Link from 'next/link'
 import React, { useState } from 'react'
 import Navbar from './Navbar'
@@ -6,18 +6,17 @@ import Navbar from './Navbar'
 
 
 const Profile = () => {
-    const [selectedId, setSelectedId] = useState(null)
   return (
 <>
 <Navbar/>
 <div className='flex bg-black'>
 
-<div className="flex flex-col  w-1/3 h-screen px-4 py-8 overflow-y-auto rtl:border-r-0 rtl:border-l  dark:border-gray-700">
+<div className="flex flex-col w-1/3 h-screen px-4 py-8 overflow-y-auto rtl:border-r-0 rtl:border-l  dark:border-gray-700">
  
     <div className="flex flex-col items-center mt-6 -mx-2">
-        <img className="object-cover w-24 h-24 mx-2 rounded-full" src="" alt="avatar"></img>
-        <h4 className="mx-2 mt-2 font-medium text-gray-800 dark:text-gray-200">XXX</h4>
-        <p className="mx-2 mt-1 text-sm font-medium text-gray-600 dark:text-gray-400">XXXS</p>
+        <img className="object-cover w-40 h-40 mx-2 rounded-full" src="https://i.pravatar.cc/150?img=69 " alt="avatar"></img>
+        <h4 className="mx-2 text-lg mt-4 font-bold text-slate-50">User</h4>
+        <p className="mx-2 mt-1 text-md font-medium  text-slate-50">UserId</p>
     </div>
 
     <div className="flex flex-col justify-between flex-1 mt-6">
@@ -41,14 +40,7 @@ const Profile = () => {
                 <span className="mx-4 font-medium">Rewards</span>
             </Link>
 
-            <Link className="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-lg dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700" href="#">
-                <svg className="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M10.3246 4.31731C10.751 2.5609 13.249 2.5609 13.6754 4.31731C13.9508 5.45193 15.2507 5.99038 16.2478 5.38285C17.7913 4.44239 19.5576 6.2087 18.6172 7.75218C18.0096 8.74925 18.5481 10.0492 19.6827 10.3246C21.4391 10.751 21.4391 13.249 19.6827 13.6754C18.5481 13.9508 18.0096 15.2507 18.6172 16.2478C19.5576 17.7913 17.7913 19.5576 16.2478 18.6172C15.2507 18.0096 13.9508 18.5481 13.6754 19.6827C13.249 21.4391 10.751 21.4391 10.3246 19.6827C10.0492 18.5481 8.74926 18.0096 7.75219 18.6172C6.2087 19.5576 4.44239 17.7913 5.38285 16.2478C5.99038 15.2507 5.45193 13.9508 4.31731 13.6754C2.5609 13.249 2.5609 10.751 4.31731 10.3246C5.45193 10.0492 5.99037 8.74926 5.38285 7.75218C4.44239 6.2087 6.2087 4.44239 7.75219 5.38285C8.74926 5.99037 10.0492 5.45193 10.3246 4.31731Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M15 12C15 13.6569 13.6569 15 12 15C10.3431 15 9 13.6569 9 12C9 10.3431 10.3431 9 12 9C13.6569 9 15 10.3431 15 12Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
 
-                <span className="mx-4 font-medium">Settings</span>
-            </Link>
         </nav>
     </div>
 </div>
@@ -62,10 +54,17 @@ const Profile = () => {
 
 
 
-<div className='flex flex-col w-2/3 m-10 items-center justify-center align-middle'>
+<div className='flex flex-col w-1/2 m-10 mx-auto items-center justify-center align-middle'>
     <div className='text-white text-5xl font-light mb-10'>
         Claim your Rewards!
     </div>
+    <AnimatePresence>
+    <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -50 }}
+          transition={{ duration: 0.5 }}
+        >
     <motion.a
   whileHover={{ scale: 1.1 }}
   onHoverStart={e => {}}
@@ -105,8 +104,17 @@ const Profile = () => {
   </div>
 </article>
   </motion.a>
+  </motion.div>
+  
+  
+  <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -50 }}
+          transition={{ duration: 0.5 ,delay:0.1}}
+        >
   <motion.a
-  whileHover={{ scale: 1.1 }}
+  whileHover={{ scale: 1.2 }}
   onHoverStart={e => {}}
   onHoverEnd={e => {}}
 >
@@ -144,7 +152,16 @@ const Profile = () => {
   </div>
 </article>
 </motion.a>
+</motion.div>
 
+
+<motion.div
+
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -50 }}
+          transition={{ duration: 0.5 ,delay:0.2}}
+        >
 <motion.a
   whileHover={{ scale: 1.1 }}
   onHoverStart={e => {}}
@@ -184,6 +201,9 @@ const Profile = () => {
   </div>
 </article>
 </motion.a>
+</motion.div>
+
+</AnimatePresence>
 
 
 </div>
